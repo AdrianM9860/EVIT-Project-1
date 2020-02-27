ee5c0876c04c42c623c464e8919598d48bccca39
AdrianM9860 <60239066+AdrianM9860@users.noreply.github.com>
2020-02-27 13:48:49 -0700

    Add files via upload

--- ClothingSimulatorProject/ClothingSimulator/Form1.Designer.cs
+++ ClothingSimulatorProject/ClothingSimulator/Form1.Designer.cs
@@ -28,13 +28,372 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.shop1 = new System.Windows.Forms.Panel();
+            this.lblCurrencyPoints1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.miniGameTab = new System.Windows.Forms.Panel();
+            this.itemsTab = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.name1 = new System.Windows.Forms.PictureBox();
+            this.title = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblCurrencyPoints = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnShopTab = new System.Windows.Forms.Button();
+            this.btnMiniGame = new System.Windows.Forms.Button();
+            this.btnExit2 = new System.Windows.Forms.Button();
+            this.itemTab = new System.Windows.Forms.Button();
+            this.btnMiniGameStart = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.shop1.SuspendLayout();
+            this.miniGameTab.SuspendLayout();
+            this.itemsTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.name1)).BeginInit();
+            this.title.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // shop1
+            // 
+            this.shop1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.shop1.Controls.Add(this.lblCurrencyPoints1);
+            this.shop1.Controls.Add(this.label1);
+            this.shop1.Location = new System.Drawing.Point(0, 37);
+            this.shop1.Name = "shop1";
+            this.shop1.Size = new System.Drawing.Size(784, 433);
+            this.shop1.TabIndex = 0;
+            // 
+            // lblCurrencyPoints1
+            // 
+            this.lblCurrencyPoints1.AutoSize = true;
+            this.lblCurrencyPoints1.Location = new System.Drawing.Point(123, 11);
+            this.lblCurrencyPoints1.Name = "lblCurrencyPoints1";
+            this.lblCurrencyPoints1.Size = new System.Drawing.Size(13, 13);
+            this.lblCurrencyPoints1.TabIndex = 1;
+            this.lblCurrencyPoints1.Text = "a";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Currency Points: ";
+            // 
+            // miniGameTab
+            // 
+            this.miniGameTab.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.miniGameTab.Controls.Add(this.label2);
+            this.miniGameTab.Controls.Add(this.label3);
+            this.miniGameTab.Location = new System.Drawing.Point(802, 37);
+            this.miniGameTab.Name = "miniGameTab";
+            this.miniGameTab.Size = new System.Drawing.Size(784, 433);
+            this.miniGameTab.TabIndex = 2;
+            // 
+            // itemsTab
+            // 
+            this.itemsTab.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.itemsTab.Controls.Add(this.btnRoll);
+            this.itemsTab.Controls.Add(this.groupBox3);
+            this.itemsTab.Controls.Add(this.groupBox2);
+            this.itemsTab.Controls.Add(this.groupBox1);
+            this.itemsTab.Controls.Add(this.btnMiniGameStart);
+            this.itemsTab.Controls.Add(this.label4);
+            this.itemsTab.Controls.Add(this.label5);
+            this.itemsTab.Location = new System.Drawing.Point(802, 37);
+            this.itemsTab.Name = "itemsTab";
+            this.itemsTab.Size = new System.Drawing.Size(784, 433);
+            this.itemsTab.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(123, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "a";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Currency Points: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "a";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Currency Points: ";
+            // 
+            // name1
+            // 
+            this.name1.BackColor = System.Drawing.Color.Transparent;
+            this.name1.Image = ((System.Drawing.Image)(resources.GetObject("name1.Image")));
+            this.name1.InitialImage = ((System.Drawing.Image)(resources.GetObject("name1.InitialImage")));
+            this.name1.Location = new System.Drawing.Point(173, 15);
+            this.name1.Name = "name1";
+            this.name1.Size = new System.Drawing.Size(432, 405);
+            this.name1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.name1.TabIndex = 0;
+            this.name1.TabStop = false;
+            this.name1.Click += new System.EventHandler(this.name1_Click);
+            // 
+            // title
+            // 
+            this.title.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.title.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.title.Controls.Add(this.btnExit);
+            this.title.Controls.Add(this.lblCurrencyPoints);
+            this.title.Controls.Add(this.btnStart);
+            this.title.Controls.Add(this.name1);
+            this.title.Location = new System.Drawing.Point(0, 79);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(784, 391);
+            this.title.TabIndex = 2;
+            this.title.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(285, 385);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(180, 45);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblCurrencyPoints
+            // 
+            this.lblCurrencyPoints.AutoSize = true;
+            this.lblCurrencyPoints.Location = new System.Drawing.Point(108, 15);
+            this.lblCurrencyPoints.Name = "lblCurrencyPoints";
+            this.lblCurrencyPoints.Size = new System.Drawing.Size(0, 13);
+            this.lblCurrencyPoints.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(269, 328);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(206, 55);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnShopTab
+            // 
+            this.btnShopTab.Location = new System.Drawing.Point(12, 8);
+            this.btnShopTab.Name = "btnShopTab";
+            this.btnShopTab.Size = new System.Drawing.Size(75, 23);
+            this.btnShopTab.TabIndex = 1;
+            this.btnShopTab.Text = "Shop";
+            this.btnShopTab.UseVisualStyleBackColor = true;
+            this.btnShopTab.Click += new System.EventHandler(this.btnShopTab_Click);
+            // 
+            // btnMiniGame
+            // 
+            this.btnMiniGame.Location = new System.Drawing.Point(93, 8);
+            this.btnMiniGame.Name = "btnMiniGame";
+            this.btnMiniGame.Size = new System.Drawing.Size(75, 23);
+            this.btnMiniGame.TabIndex = 3;
+            this.btnMiniGame.Text = "Minigame";
+            this.btnMiniGame.UseVisualStyleBackColor = true;
+            this.btnMiniGame.Click += new System.EventHandler(this.btnMiniGame_Click);
+            // 
+            // btnExit2
+            // 
+            this.btnExit2.Location = new System.Drawing.Point(703, 8);
+            this.btnExit2.Name = "btnExit2";
+            this.btnExit2.Size = new System.Drawing.Size(75, 23);
+            this.btnExit2.TabIndex = 4;
+            this.btnExit2.Text = "Exit";
+            this.btnExit2.UseVisualStyleBackColor = true;
+            this.btnExit2.Click += new System.EventHandler(this.btnExit2_Click);
+            // 
+            // itemTab
+            // 
+            this.itemTab.Location = new System.Drawing.Point(174, 8);
+            this.itemTab.Name = "itemTab";
+            this.itemTab.Size = new System.Drawing.Size(75, 23);
+            this.itemTab.TabIndex = 5;
+            this.itemTab.Text = "Items";
+            this.itemTab.UseVisualStyleBackColor = true;
+            this.itemTab.Click += new System.EventHandler(this.itemTab_Click);
+            // 
+            // btnMiniGameStart
+            // 
+            this.btnMiniGameStart.Location = new System.Drawing.Point(306, 295);
+            this.btnMiniGameStart.Name = "btnMiniGameStart";
+            this.btnMiniGameStart.Size = new System.Drawing.Size(75, 23);
+            this.btnMiniGameStart.TabIndex = 2;
+            this.btnMiniGameStart.Text = "START";
+            this.btnMiniGameStart.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(242, 83);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(58, 155);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(306, 83);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(58, 155);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Location = new System.Drawing.Point(370, 83);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(58, 155);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(10, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 39);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "N";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(10, 59);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 39);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "N";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(10, 59);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 39);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "N";
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Location = new System.Drawing.Point(289, 244);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(99, 45);
+            this.btnRoll.TabIndex = 5;
+            this.btnRoll.Text = "ROLL";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1604, 470);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.itemTab);
+            this.Controls.Add(this.itemsTab);
+            this.Controls.Add(this.miniGameTab);
+            this.Controls.Add(this.btnMiniGame);
+            this.Controls.Add(this.btnShopTab);
+            this.Controls.Add(this.btnExit2);
+            this.Controls.Add(this.shop1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.TopMost = true;
+            this.shop1.ResumeLayout(false);
+            this.shop1.PerformLayout();
+            this.miniGameTab.ResumeLayout(false);
+            this.miniGameTab.PerformLayout();
+            this.itemsTab.ResumeLayout(false);
+            this.itemsTab.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.name1)).EndInit();
+            this.title.ResumeLayout(false);
+            this.title.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel shop1;
+        private System.Windows.Forms.PictureBox name1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel title;
+        private System.Windows.Forms.Label lblCurrencyPoints;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnShopTab;
+        private System.Windows.Forms.Button btnMiniGame;
+        private System.Windows.Forms.Label lblCurrencyPoints1;
+        private System.Windows.Forms.Button btnExit2;
+        private System.Windows.Forms.Button itemTab;
+        private System.Windows.Forms.Panel miniGameTab;
+        private System.Windows.Forms.Panel itemsTab;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnMiniGameStart;
     }
 }
 
